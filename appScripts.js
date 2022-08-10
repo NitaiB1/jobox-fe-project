9c0233c757774d607ecb19d48431d106f68b3b4c
Nitai Baboolal <91281480+NitaiB1@users.noreply.github.com>
2022-08-10 02:31:44 +0200

    Add files via upload

---
+++ appScripts.js
@@ -0,0 +1,25 @@
+//this is to do form validation
+function check(){
+    var err = ''
+    var errmsg = "The following Fields are empty: <br>"
+    if($('#clientName').val() == ""){
+        err = err + "Client name. <br>";
+    }
+    if($('#contractDateStart').val() == ""){
+        err = err + "Contract start date. <br>";
+    }
+    if($('#contractDateEnd').val() == ""){
+        err = err + "Contract end date. <br>";
+    }
+    if($('#serviceCost').val() == ""){
+        err = err + "Service cost. <br>";
+    }
+    if($('#serviceDesc').val() == ""){
+        err = err + "Service description. <br>";
+    }
+    if(err!=''){
+        $('#errors').html('<div class="alert alert-danger" role="alert">'+errmsg+err+'</div>');
+    }else{
+        
+    }
+}
