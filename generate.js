0e8550c72f4d4c162c5433c00ac1175519e00b37
NitaiB1 <baboolalnitai@gmail.com>
2022-08-10 15:09:32 +0200

    attempting to fix production bugs

--- generate.js
+++ generate.js
@@ -5,14 +5,12 @@ function generate() {
             children: [
                 new docx.Paragraph({
                     children: [
-                        new docx.TextRun("Hello World"),
+                        new docx.TextRun($('#clientName').val()),
                         new docx.TextRun({
-                            text: "Foo Bar",
-                            bold: true,
+                            
                         }),
                         new docx.TextRun({
-                            text: "\tGithub is the best",
-                            bold: true,
+                            
                         }),
                     ],
                 }),
